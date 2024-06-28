6cdcf5907dd2e436d372215e5152eacf892801b2
Shakh7 <ssharipov3@students.wiut.uz>
2024-06-28 18:54:15 +0500

    images.domains

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: process.env.NEXT_PUBLIC_DISABLE_ESLINT === 'true',
     },
+    images: {
+        domains: ['images.unsplash.com', 'raw.githubusercontent.com'],
+    },
 };
 
 export default nextConfig;
