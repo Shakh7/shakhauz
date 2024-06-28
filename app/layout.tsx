24463d83b8b03da0ea86e5292dbd677178ca19fc
Shakh7 <ssharipov3@students.wiut.uz>
2024-06-28 19:15:02 +0500

    openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata: Metadata = {
     title: {
         default: "Shakhzodbek Sharipov",
+
         template: "%s | Shakhzodbek Sharipov"
     },
     description: "Shakhzodbek Sharipov's Personal Portfolio",
